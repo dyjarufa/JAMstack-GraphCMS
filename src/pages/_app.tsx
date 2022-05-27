665aeddd05067d32dbff24e2df203a63eca1591a
jady-santos <jady.santos@vodafone.com>
2022-05-27 10:49:41 +0100

    feat: add static generation and create new queries

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,23 @@
 import { Provider } from 'urql'
-import { client } from '../lib/urql'
+import { Footer } from '../components/Footer'
+import { Header } from '../components/Header'
+import { client, ssrCache } from '../lib/urql'
 
 import '../styles/globals.css'
 
 
 function MyApp({ Component, pageProps }) {
+  // reaproveitar o cache que vem do server side da aplicação
+  // e aproveitar os dados no client side
+  if(pageProps.urlState) {
+    ssrCache.restoreData(pageProps.urlState)
+  }
+
   return (
     <Provider value={client}>
+      <Header />
       <Component {...pageProps} />
+      <Footer />
     </Provider>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,33 @@
-import { usePageQuery } from "../generated/graphql";
+import { GetServerSideProps } from "next";
+import { Features } from "../components/Features";
+import { Hero } from "../components/Hero";
+import { Pricing } from "../components/Pricing";
+import { usePageQuery, PageDocument } from "../generated/graphql";
+import { client, ssrCache } from "../lib/urql";
 
 export default function Home() {
-  const [{ data }] = usePageQuery({ 
-    variables: { 
-      slug: 'home' 
-    } 
-  })
-
-  console.log('chegou', data)
+  const [{ data }] = usePageQuery({
+    variables: {
+      slug: "home",
+    },
+  });
 
   return (
-    <h1>{data?.page.title}</h1>
-  )
+    <>
+      <Hero title={data?.page.title} subtitle={data?.page.subtitle} />
+      <Features />
+      <Pricing />
+
+    </>
+  );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  await client.query(PageDocument, { slug: "home" }).toPromise();
+
+  return {
+    props: {
+      urqlState: ssrCache.extractData(),
+    },
+  };
+};
